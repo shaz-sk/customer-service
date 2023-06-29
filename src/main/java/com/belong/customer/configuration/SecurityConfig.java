14834b0877c3ee82878209730172a2dafcecf05b
shaz-sk <shanka900@gmail.com>
2023-06-29 19:37:23 +1000

    Update version

--- src/main/java/com/belong/customer/configuration/SecurityConfig.java
+++ src/main/java/com/belong/customer/configuration/SecurityConfig.java
@@ -5,26 +5,47 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
+
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user1 = User.withDefaultPasswordEncoder().username("Joe1").password("Joe1").roles("USER").build();
-        UserDetails admin1 = User.withDefaultPasswordEncoder().username("admin1").password("admin1").roles("ADMIN").build();
-        return new InMemoryUserDetailsManager(user1, admin1);
+    public UserDetailsService userDetailsService(BCryptPasswordEncoder passwordEncoder) {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("Joe1")
+                .password(passwordEncoder.encode("Joe1"))
+                .roles("USER")
+                .build());
+        manager.createUser(User.withUsername("admin1")
+                .password(passwordEncoder.encode("admin1"))
+                .roles("USER", "ADMIN")
+                .build());
+        return manager;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().and().authorizeRequests().antMatchers(String.valueOf(HttpMethod.PUT), "/api/v1/customers/**")
-                .permitAll().anyRequest().authenticated()
-                .and().csrf().disable();
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(String.valueOf(HttpMethod.PUT), "/api/v1/customers/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic().and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);;
+        return http.build();
     }
 }
